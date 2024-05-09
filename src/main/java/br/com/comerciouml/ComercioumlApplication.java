b07df834ecf700f6dbfbdf67530b36ae92da8b61
guutooh <gustavo.oliveiradossantos@gmail.com>
2024-05-09 20:41:46 -0300

    inicio do projeto/repositorio

---
+++ src/main/java/br/com/comerciouml/ComercioumlApplication.java
@@ -0,0 +1,13 @@
+package br.com.comerciouml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercioumlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercioumlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/comerciouml/ComercioumlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.comerciouml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioumlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
