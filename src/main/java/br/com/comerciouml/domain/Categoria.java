7ff7002a5fe8fb7b8e99482b99ca98dd78e99d1c
guutooh <gustavo.oliveiradossantos@gmail.com>
2024-05-09 23:43:07 -0300

    feat - add dependencias H2,JPA e criacao da primeira entidade.

---
+++ src/main/java/br/com/comerciouml/domain/Categoria.java
@@ -0,0 +1,25 @@
+package br.com.comerciouml.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Entity
+public class Categoria implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+}
